91747eaaeacbd271e80276f4129ab87dd83c5d3b
razorback4417 <razorback4417.800@gmail.com>
2024-12-07 20:20:44 -0800

    o1 with edits

--- test_animation.py
+++ test_animation.py
@@ -4,10 +4,13 @@ class MergeSortAnimation(Scene):
     def construct(self):
         # Initial array to sort
         array = [38, 27, 43, 3, 9, 82, 10]
-        self.origin = ORIGIN
+        # Move origin up to allow space for downward splits
+        self.origin = UP * 2
 
         # Create and display the array
         array_mob = self.create_array_mob(array)
+        # Scale down the entire visualization to ensure it fits
+        array_mob.scale(0.7)
         self.play(FadeIn(array_mob))
         self.wait(1)
 
@@ -22,7 +25,8 @@ class MergeSortAnimation(Scene):
             num = Text(str(value), font_size=36)
             num.move_to(square.get_center())
             square_group = VGroup(square, num)
-            square_group.move_to(self.origin + RIGHT * i)
+            # Center the array horizontally
+            square_group.move_to(self.origin + RIGHT * (i - len(array)/2 + 0.5))
             array_mob.add(square_group)
         return array_mob
 
@@ -40,13 +44,13 @@ class MergeSortAnimation(Scene):
             self.play(right_mob.animate.set_color(GREEN))
             self.wait(0.5)
 
-            # Recursively sort the left half
-            left_target_pos = position + UP*2 - RIGHT*(len(left_array)/2)
+            # Recursively sort the left half - moving DOWN instead of UP
+            left_target_pos = position + DOWN*1.5 - RIGHT*(len(left_array)/2)
             self.play(left_mob.animate.move_to(left_target_pos))
             self.merge_sort(left_array, left_mob, left_target_pos)
 
-            # Recursively sort the right half
-            right_target_pos = position + UP*2 + RIGHT*(len(right_array)/2)
+            # Recursively sort the right half - moving DOWN instead of UP
+            right_target_pos = position + DOWN*1.5 + RIGHT*(len(right_array)/2)
             self.play(right_mob.animate.move_to(right_target_pos))
             self.merge_sort(right_array, right_mob, right_target_pos)
 
@@ -88,6 +92,7 @@ class MergeSortAnimation(Scene):
 
         # Animate merging arrays back together
         for idx, mob in enumerate(array_mob):
-            target_pos = position + RIGHT * idx
+            # Center the merged array horizontally
+            target_pos = position + RIGHT * (idx - len(array_mob)/2 + 0.5)
             self.play(mob.animate.move_to(target_pos), run_time=0.5)
         self.wait(0.5)
